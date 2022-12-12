936b527b5da1e39a265664a2564f1816d7bc4276
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2022-12-12 19:31:33 +0530

    udemy-course-add

---
+++ javascript-tutorial/udemy-javascript-course/starter/index.js
@@ -0,0 +1,62 @@
+// let javascriptIsNice = false;
+// console.log(javascriptIsNice)
+// console.log(typeof you)
+// console.log(typeof 25)
+// console.log(typeof"hii")
+// console.log(typeof'25')
+// // console.log()
+// // console.log()
+
+
+
+// var
+
+// let age =90;
+// age = 69;
+// console.log(age)
+
+// const birthYear = 1999;
+// birthYear =  2000;
+// console.log(birthYear)
+
+
+// const now = 2020;
+// const ageMan = now - 1999;
+// const ageBoy = now - 1997;
+// console.log(ageBoy , ageMan)
+
+
+// console.log(ageMan * 2, ageMan / 10,2 ** 3);
+
+// // 2**3 means 2 to the power of 3*2*2* 2
+
+// const firstName = "Jonas";
+// const lastName ="schmedmann";
+// console.log(firstName +""+ lastName);
+
+// let x = 10 + 5 ;
+// x+= 10;
+// x+= 4;
+// x--;
+// x--;
+// console.log(x);
+
+
+// console.log(ageMan > ageBoy);
+// console.log(ageBoy >= 18);
+
+
+// const isFullAge = ageBoy >= 18;
+
+// console.log(now - 1991 > now -2018 )
+
+//////
+//operator precedence
+const now = 2037;
+
+const ageMan = now - 1991;
+const ageBoy = now - 2018;
+console.log(now -1991> now -2018);
+
+let x ,y
+
