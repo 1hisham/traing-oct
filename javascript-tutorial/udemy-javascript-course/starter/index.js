afa2c392f712cfcaa1eeaac23666426790130975
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2022-12-13 19:10:54 +0530

    section-one-added-on-udemy

--- javascript-tutorial/udemy-javascript-course/starter/index.js
+++ javascript-tutorial/udemy-javascript-course/starter/index.js
@@ -52,11 +52,175 @@
 
 //////
 //operator precedence
-const now = 2037;
+// const now = 2037;
 
-const ageMan = now - 1991;
-const ageBoy = now - 2018;
-console.log(now -1991> now -2018);
+// const ageMan = now - 1991;
+// const ageBoy = now - 2018;
+// console.log(now -1991> now -2018);
 
-let x ,y
+// let x ,y
+
+// const now =2037;
+// const ageJonas =now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// // console.log(25-10-5);
+// let x , y;
+// x= y= 25 -10 -5 ;
+
+// console.log(x,y); 
+
+
+// let mass = 15;
+// let height = 1.3;
+
+// let bmi = mass / height ** 2 
+// // = masss / (height * height)
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.92;
+
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// if(BMIJohn > BMIMark){
+//     console.log(`${BMIJohn} john have higher BMI` )
+
+// }else{
+//     console.log(`${BMIMark} mark have higher BMI`)
+// }
+
+// const markHigherBMI = BMIMark <  BMIJohn;
+
+// console.log(BMIJohn , BMIMark , markHigherBMI);
+
+// const firstName = "jonas";
+// const job = "teacher"
+// const birthYear = 1999;
+// const year = 2022;
+
+// const jonas = " iam " + firstName +" , a " + (year - birthYear) + "year old" + job + "!";
+// const jonasNew = `iam ${firstName} , a ${year - birthYear} ylear old ${job}!`;
+
+// console.log(jonas)
+
+// console.log(jonasNew)
+
+// const age = 14;
+// const saOldEnough = age >=18;
+
+// console.log(saOldEnough);
+
+// console.log("sting with\n\ miltiple\n\lines")
+// console.log(`strisnbgs
+// ikjs
+// allkj
+// kgal`)
+
+// const age = 15;
+
+// if (age >= 18){
+//     console.log(`Sarah can start driving licvense`);
+// }else {
+//     const yearLeft = 18 - age;
+//     console.log(`sasrah is too young . Wait another ${yearLeft}
+//     `)}
+    
+//     let century;
+//     const birthYear = 2012;
+//     if (birthYear<= 2000){
+//         century =`21th century`
+//     }else{
+//         century =`20th century`
+// }
+
+// console.log(century)
+
+
+// const inputYear = "1991";
+// console.log(Number(inputYear),inputYear);
+// console.log(Number(inputYear) +18);
+// console.log(Number('johns'))
+
+// console.log(typeof NaN);
+
+// console.log(String(23),23 );
+
+// console.log("I am "+ 23 +"year old")
+// console.log ( '23' - '10' -3)
+// console.log('23' * '2');
+
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n);
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('johns'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+// console.log(Boolean(null))
+
+// const money = 0;
+// if (money){
+//     console.log(`don't spant all`)
+// }else{
+//     console.log(`you should get a job`)
+// }
+
+// let heigt;
+// if (heigt){
+// console.log(`you are tall`)
+// }else {
+// console.log('you are samll')
+// }
+
+
+// const favourite = Number(prompt("what's your favourate number!"))
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite === 23){
+//     console.log('cool ! 23 IS A AMAZING NUMBER')
+// }else if (favourite ===7){
+//     console.log(`9 is also cool`)
+// }else if (favourite === 9){
+//     console.log(`9 is cool number`)
+// }else{
+//     console.log(`number is not 23 or 7 or 9`)
+    
+// }
+
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense ||  hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// if(hasDriversLicense && hasGoodVision){
+//     console.log(`sarha is able to drive!`);
+
+// }else {
+//     console.log(`someone else should drive`);
+
+
+// }
+
+// const isTired = false ;
+// console.log(hasDriversLicense && hasGoodVision && isTired); 
+
+// if(hasDriversLicense && hasGoodVision && !isTired){
+//     console.log(`sarha is able to drive!`);
+
+// }else {
+//     console.log(`someone else should drive`);
+// }
+
+
 
