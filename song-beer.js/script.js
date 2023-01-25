51bd7c0c023e7afd3f1b594909f31e7dc668c2a2
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-01-25 20:57:59 +0530

    small projecst

---
+++ song-beer.js/script.js
@@ -0,0 +1,36 @@
+const text = document.querySelector(".text");
+
+let first = "";
+let second;
+let flag = 0;
+let bottleCountOnWall = "";
+let bottleCountAfter = "";
+
+for (let i = 99; 0 <= i; i--) {
+    let b = i - 1;
+    if (i === 0) {
+    bottleCountOnWall = "No bottles";
+  } else if (i === 1) {
+    bottleCountOnWall = "1 bottle";
+  } else {
+    bottleCountOnWall = i.toString() + " bottles";
+  }
+  if (bottleCountAfter < 1) {
+    text.innerHTML = `<span>No More bottles of beer on the wall, bottles of beer.<br>Go to the store and buy some more , 99 bottles of beer on the wall.</span> `;
+  }
+  bottleCountAfter = b.toString();
+
+  if (bottleCountAfter === 0) {
+    bottleCountAfter = "no bottle";
+  }
+
+  second = ` <span><br>${bottleCountOnWall} of beer on the wall, ${bottleCountOnWall}  of beer.<br>Take one down and pass it around, ${bottleCountAfter} of beer on the wall.<br></span> `;
+  first += second;
+
+  text.innerHTML = first;
+}
+
+//   if (flag === 0) {
+//     console.log(first);
+//     flag = 1;
+//   }
