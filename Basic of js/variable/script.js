a8a62929fe5bfce632a301ae5821b087e426ea3f
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-01-27 21:26:40 +0530

    basics of js

---
+++ Basic of js/variable/script.js
@@ -0,0 +1,58 @@
+// let myVariable= "bob"
+// myVariable = 'stive'
+
+// console.log(myVariable);
+
+
+///CONDITIONS
+
+// let iceCream = 'chocolate';
+
+// if(iceCream === 'chocolate'){
+//     alert('yah i need cholate ice cream')
+
+// }else {
+//     alert('chocloate is my favourate but its ok')
+// }
+
+
+// FUNCTION 
+
+// function multiply(num1,num2){
+//     let result = num1 * num2;
+//     return  result;
+// }
+
+
+// console.log(multiply(4,6));
+
+
+// function map(f, a) {
+//     const result = new Array(a.length);
+//     for (let i = 0; i < a.length; i++) {
+//       result[i] = f(a[i]);
+//     }
+//     return result;
+// }
+
+// const f = function (x) {
+//       return x * x * x;
+//   }
+  
+//   const numbers = [0, 1, 2, 5, 10];
+//   const cube = map(f, numbers);
+//   console.log(cube);
+
+
+  function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    } 
+
+
+  } 
+  
+  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
