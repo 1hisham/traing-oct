3f3d7535f15b61215c98cd4f6c41ce54b0f3737d
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-01-29 20:06:46 +0530

    project song

--- song-beer.js/script.js
+++ song-beer.js/script.js
@@ -9,26 +9,28 @@ let bottleCountAfter = "";
 for (let i = 99; 0 <= i; i--) {
     let b = i - 1;
     if (i === 0) {
-    bottleCountOnWall = "No bottles";
-  } else if (i === 1) {
-    bottleCountOnWall = "1 bottle";
-  } else {
-    bottleCountOnWall = i.toString() + " bottles";
-  }
-  if (bottleCountAfter < 1) {
-    text.innerHTML = `<span>No More bottles of beer on the wall, bottles of beer.<br>Go to the store and buy some more , 99 bottles of beer on the wall.</span> `;
-  }
-  bottleCountAfter = b.toString();
-
-  if (bottleCountAfter === 0) {
-    bottleCountAfter = "no bottle";
-  }
-
-  second = ` <span><br>${bottleCountOnWall} of beer on the wall, ${bottleCountOnWall}  of beer.<br>Take one down and pass it around, ${bottleCountAfter} of beer on the wall.<br></span> `;
-  first += second;
-
-  text.innerHTML = first;
+        bottleCountOnWall = "No bottles";
+    } else if (i === 1) {
+        bottleCountOnWall = "1 bottle";
+    } else {
+        bottleCountOnWall = i.toString() + " bottles";
+    }
+    bottleCountAfter = b.toString();
+    
+    
+    if (bottleCountAfter === 0) {
+        bottleCountAfter = "no bottle";
+    }
+    
+    second = ` <span><br>${bottleCountOnWall} of beer on the wall, ${bottleCountOnWall}  of beer.<br>Take one down and pass it around, ${bottleCountAfter} of beer on the wall.<br></span> `;
+   
+    if (bottleCountAfter < 0) {
+        second = `<span><br>No More bottles of beer on the wall, bottles of beer.<br>Go to the store and buy some more , 99 bottles of beer on the wall.</span> `;
+    }
+    first += second;
+    
 }
+text.innerHTML = first;
 
 //   if (flag === 0) {
 //     console.log(first);
