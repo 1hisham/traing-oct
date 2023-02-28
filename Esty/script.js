2fca4b36ba09003d3a093b228b8cfe671001f11c
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-02-28 22:26:32 +0530

    filpkart modified

--- Esty/script.js
+++ Esty/script.js
@@ -13,11 +13,11 @@ const inputValue = document.querySelector(".toget-input");
 const buttonInput = document.querySelector(".input-button");
 const buttonClear = document.querySelector(".clear-button");
 
-function totalCategorylist() {
-  function categorylist() {
-    const categorylist = {
-      sales: "Seller's Sales Hub",
-      jewellery: "jewellery & Accessories",
+
+function categorylist() {
+  const categorylist = {
+    sales: "Seller's Sales Hub",
+    jewellery: "jewellery & Accessories",
       wears: "Clothing & Shoes",
       home: "Home & Living",
       wedding: "Wedding & Party",
@@ -28,7 +28,7 @@ function totalCategorylist() {
     };
     return categorylist;
   }
-
+  
   function categorylistItteration() {
     categorylist = categorylist();
     for (const key in categorylist) {
@@ -44,36 +44,37 @@ function totalCategorylist() {
   }
 
   categorylistItteration();
-}
-
-totalCategorylist();
-
-// second section
-function totalDiscoverList() {
-  fetch(
-    "https://raw.githubusercontent.com/1hisham/traing-oct/main/server/discoverlist.json"
-  )
-    .then((response) => response.json())
-    .then((discoverlist) => {
-      function discoverListItteration() {
-        for (const key in discoverlist) {
-          let input = document.createElement("li");
-          discoverItems.appendChild(input);
-          input.innerHTML = `<img src="${discoverlist[key].imglink}" alt="">`;
-          let imgText = document.createElement("span");
-          input.appendChild(imgText);
+  
+  
+  
+  
+  // second section
+  function totalDiscoverList() {
+    fetch(
+      "https://raw.githubusercontent.com/1hisham/traing-oct/main/server/discoverlist.json"
+      )
+      .then((response) => response.json())
+      .then((discoverlist) => {
+        function discoverListItteration() {
+          for (const key in discoverlist) {
+            let input = document.createElement("li");
+            discoverItems.appendChild(input);
+            input.innerHTML = `<img src="${discoverlist[key].imglink}" alt="">`;
+            let imgText = document.createElement("span");
+            input.appendChild(imgText);
           imgText.innerHTML = discoverlist[key].caption;
         }
       }
       discoverListItteration();
     });
-}
-totalDiscoverList();
-
-function totalDealOfDay() {
-  fetch(
+  }
+  totalDiscoverList();
+  
+  
+  function totalDealOfDay() {
+    fetch(
     "https://raw.githubusercontent.com/1hisham/traing-oct/main/server/imageCardsItems.json"
-  )
+    )
     .then((response) => response.json())
     .then((imgaeCardsItems) => {
       function deaOfDay() {
@@ -98,102 +99,128 @@ function totalDealOfDay() {
 
       deaOfDay();
     });
-}
+  }
+  
+  totalDealOfDay();
+  
+  //Popular gift right now 
+  
+  //  DISCOUNT PRICE
+  
+  
+  
+  function calculationDiscount(products) {
+    let discountPrice = 0;
+    let price = 0;
+    let priceArray=[]
+    products.forEach(item =>{
+      console.log(item , "items");
+      
+      discountPrice =
+      item.amount.originalPrice *
+      (item.amount.discount / 100);
+      console.log(item.amount.originalPrice);
+      price = item.amount.originalPrice - discountPrice;
+      item.amount.payPrice = price;
+      console.log(price);
+      priceArray.push(Math.floor(price));
+      
+    })
+    return price
 
-totalDealOfDay();
-
-function totalPopularGiftsNow() {
-  fetch(
-    "https://raw.githubusercontent.com/1hisham/traing-oct/main/server/popularGiftsNow.json"
-  )
-    .then((response) => response.json())
-    .then((popularGiftsNow) => {
-      //  DISCOUNT PRICE
-
-      let sortedArray;
-      let priceArray = [];
-
-      function calculationDiscount() {
-        let discountPrice = 0;
-        let price = 0;
-        for (let i = 0; i < popularGiftsNow.length; i++) {
-          discountPrice =
-            popularGiftsNow[i].amount.originalPrice *
-            (popularGiftsNow[i].amount.discount / 100);
-          console.log(popularGiftsNow[i].amount.originalPrice);
-          price = popularGiftsNow[i].amount.originalPrice - discountPrice;
-          popularGiftsNow[i].amount.payPrice = price;
-          console.log(price);
-          priceArray.push(Math.floor(price));
-        }
-
-        buttonInput.addEventListener("click", () => {
-          console.log(sortedArray, "sortedArray");
-
-          sortedArray = popularGiftsNow;
-          let searchNumber = inputValue.value;
-          console.log(sortedArray, "sorted");
-          sortedArray = popularGiftsNow.filter((item, idx) => {
-            return searchNumber < item.amount.payPrice;
-          });
-
-          console.log(sortedArray);
-
-          if (popularGiftsNow !== sortedArray) {
-            popularGiftsNow = sortedArray;
-            console.log(popularGiftsNow);
-            sinlgeCard.innerHTML = "";
-            updatingFunction(popularGiftsNow);
-          }
-        });
-
-        return priceArray;
+  }
+    
+    
+    
+    
+  // star ratinig
+  
+  
+  function starRatingAmount(key) {
+    svg = "";
+      
+    for(let i =0; i< key; i++){
+        svg =
+        svg +
+          `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false" width="18" height="18"><path d="M19.985,10.36a0.5,0.5,0,0,0-.477-0.352H14.157L12.488,4.366a0.5,0.5,0,0,0-.962,0l-1.67,5.642H4.5a0.5,0.5,0,0,0-.279.911L8.53,13.991l-1.5,5.328a0.5,0.5,0,0,0,.741.6l4.231-2.935,4.215,2.935a0.5,0.5,0,0,0,.743-0.6l-1.484-5.328,4.306-3.074A0.5,0.5,0,0,0,19.985,10.36Z" fill="#222222"></path></svg>`;
       }
-
-      calculationDiscount();
-
-      // star ratinig
-
-      //
-      function starRatingAmount(key) {
-        svg = "";
-
-        for (
-          let j = 0;
-          j < Math.floor(popularGiftsNow[key].rating.stars);
-          j++
-        ) {
-          svg =
-            svg +
-            `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false" width="18" height="18"><path d="M19.985,10.36a0.5,0.5,0,0,0-.477-0.352H14.157L12.488,4.366a0.5,0.5,0,0,0-.962,0l-1.67,5.642H4.5a0.5,0.5,0,0,0-.279.911L8.53,13.991l-1.5,5.328a0.5,0.5,0,0,0,.741.6l4.231-2.935,4.215,2.935a0.5,0.5,0,0,0,.743-0.6l-1.484-5.328,4.306-3.074A0.5,0.5,0,0,0,19.985,10.36Z" fill="#222222"></path></svg>`;
-        }
-        if (popularGiftsNow[key].rating.stars % 1 !== 0) {
-          svg = svg + svgHalf;
-          return svg;
-        }
-
+      if (key % 1 !== 0) {
+        svg = svg + svgHalf;
         return svg;
       }
 
-      // POPULAR-GIFT
-      let heartEmpty = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" width="24" height="24"><path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z" fill="#222222"></path></svg>`;
-      let heartFilled = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" width="24" height="24"><path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z" fill="#A61A2E"></path></svg>`;
+      return svg;
+  }
+  
+    
+    
 
-      const headingOnPopularCard = document.createElement("h2");
-      popularGift.appendChild(headingOnPopularCard);
-      headingOnPopularCard.innerHTML = "Popular gifts right now";
 
+
+
+
+
+
+    function apiFunction(){
+      
+      fetch(
+        "https://raw.githubusercontent.com/1hisham/traing-oct/main/server/popularGiftsNow.json"
+        )
+        .then((response) => response.json())
+        .then((popularGiftsNow) => {
+
+          calculationDiscount(popularGiftsNow);
+          
+          starRatingAmount(popularGiftsNow);
+          
+          updatingFunction(popularGiftsNow)
+
+        })
+    }
+    apiFunction()
+          
+          
+          
+        //   buttonInput.addEventListener("click", () => {
+        //     console.log(sortedArray, "sortedArray");
+            
+        //     sortedArray = popularGiftsNow;
+        //     let searchNumber = inputValue.value;
+        //     console.log(sortedArray, "sorted");
+        //     sortedArray = popularGiftsNow.filter((item) => {
+        //       return searchNumber < item.amount.payPrice;
+        //     });
+          
+        //   console.log(sortedArray);
+          
+        //   if (popularGiftsNow !== sortedArray) {
+        //     popularGiftsNow = sortedArray;
+        //     console.log(popularGiftsNow);
+        //     sinlgeCard.innerHTML = "";
+        //     updatingFunction(popularGiftsNow);
+        //   }
+        // });
+        
+        
+        // POPULAR-GIFT
+        let heartEmpty = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" width="24" height="24"><path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z" fill="#222222"></path></svg>`;
+        let heartFilled = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" width="24" height="24"><path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z" fill="#A61A2E"></path></svg>`;
+     
+      
+      
+        
+        const headingOnPopularCard = document.createElement("h2");
+        popularGift.appendChild(headingOnPopularCard);
+        headingOnPopularCard.innerHTML = "Popular gifts right now";
+      
       function popularGiftCardSingle(
-        imageUrl,
-        imageDescription,
-        starRatings,
-        totalRatings,
-        symbols,
-        payPrice,
-        prices,
-        discounts,
-        freeDeliverys
-      ) {
+       item,
+       totalRatings
+       
+
+      ) 
+      {
+        
         const fullCard = document.createElement("li");
         fullCard.classList.add("full-card");
         sinlgeCard.appendChild(fullCard);
@@ -203,7 +230,7 @@ function totalPopularGiftsNow() {
         fullCard.appendChild(imageContainer);
         const image = document.createElement("img");
         imageContainer.appendChild(image);
-        image.src = imageUrl;
+        image.src = item.imageURl;
         const heartBtn = document.createElement("div");
         heartBtn.classList.add("heart-btn");
         imageContainer.appendChild(heartBtn);
@@ -224,7 +251,7 @@ function totalPopularGiftsNow() {
 
         const imageText = document.createElement("h2");
         bottomContainer.appendChild(imageText);
-        imageText.innerHTML = imageDescription;
+        imageText.innerHTML = item.caption;
 
         const rating = document.createElement("div");
         bottomContainer.appendChild(rating);
@@ -232,7 +259,7 @@ function totalPopularGiftsNow() {
         rating.classList.add("rating");
         const starRating = document.createElement("div");
         rating.appendChild(starRating);
-        starRating.innerHTML = starRatings;
+        starRating.innerHTML = item.rating.amountofRating;
 
         const totalRating = document.createElement("span");
         rating.appendChild(totalRating);
@@ -266,39 +293,23 @@ function totalPopularGiftsNow() {
       let svgHalf = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path class="foreground" d="M12 4c-.224 0-.42.15-.48.366l-1.67 5.642H4.5c-.218.002-.41.145-.472.354-.064.208.014.433.193.557l4.307 3.07-1.5 5.33c-.08.202-.02.433.15.57.17.14.41.15.59.03L12 16.98V4z"></path></svg>`;
       let svg = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false" width="18" height="18"><path d="M19.985,10.36a0.5,0.5,0,0,0-.477-0.352H14.157L12.488,4.366a0.5,0.5,0,0,0-.962,0l-1.67,5.642H4.5a0.5,0.5,0,0,0-.279.911L8.53,13.991l-1.5,5.328a0.5,0.5,0,0,0,.741.6l4.231-2.935,4.215,2.935a0.5,0.5,0,0,0,.743-0.6l-1.484-5.328,4.306-3.074A0.5,0.5,0,0,0,19.985,10.36Z" fill="#222222"></path></svg>`;
 
-      function updatingFunction(newPopular) {
-        for (let key in popularGiftsNow) {
+      function updatingFunction(products) {
+        products.forEach(item =>{
+          
+          console.log(item,"toatl ita,s");
           popularGiftCardSingle(
-            popularGiftsNow[key].imgURL,
-            popularGiftsNow[key].caption,
-            starRatingAmount([key]),
-            popularGiftsNow[key].rating.amountOfRating,
-            popularGiftsNow[key].amount.symbol,
-            popularGiftsNow[key].amount.payPrice,
-            popularGiftsNow[key].amount.originalPrice,
-            popularGiftsNow[key].amount.discount,
-            popularGiftsNow[key].freeDelivery
+            item,
+            starRatingAmount(item.rating.stars),
+            calculationDiscount()
+           
           );
-        }
-      }
+        })
+    }
+    
 
-      for (let key in popularGiftsNow) {
-        popularGiftCardSingle(
-          popularGiftsNow[key].imgURL,
-          popularGiftsNow[key].caption,
-          starRatingAmount([key]),
-          popularGiftsNow[key].rating.amountOfRating,
-          popularGiftsNow[key].amount.symbol,
-          popularGiftsNow[key].amount.payPrice,
-          popularGiftsNow[key].amount.originalPrice,
-          popularGiftsNow[key].amount.discount,
-          popularGiftsNow[key].freeDelivery
-        );
-      }
-    });
-}
 
-totalPopularGiftsNow();
+
+
 
 function singleCardOfShopSelection(imageUrl, caption) {
   const card = document.createElement("div");
