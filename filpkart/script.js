02d9fff1896aad1fcdded89561a4e73efb59e10a
Ahmmed Hisham <hisham1ahmmed@gmail.com>
2023-03-12 14:25:59 +0530

    flipkart modified

--- filpkart/script.js
+++ filpkart/script.js
@@ -1,195 +1,206 @@
-let totalAll = [
-  {
-    brandName: "Apple",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/k9loccw0/mobile/6/8/g/apple-iphone-se-mxvv2hn-a-original-imafrcqmfh4zznmq.jpeg?q=70",
-    phoneDiscription:
-      "Apple iPhone SE (White, 256 GB) (Includes EarPods, Power Adapter)",
-    rating: { starRating: "4.5", totalRating: 1500, reviews: 5984 },
-    phoneDetails: [
-      "256 GB ROM",
-      "11.94 cm (4.7 inch) Retina HD Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "A13 Bionic Chip with 3rd Gen Neural Engine Processor",
-      "Water and Dust Resistant (1 meter for Upto 30 minutes, IP67)",
-      "Fast Charge Capable",
-      "Wireless charging (Works with Qi Chargers | Qi Chargers are Sold Separately",
-      "Brand Warranty for 1 Year",
-    ],
-    price: {
-      payPrice: 0,
-      price: 40000,
-      exchangePrice: 15000,
-      discount: 9,
-    },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "Apple",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/b/u/f/-original-imaghxa5hvapbfds.jpeg?q=70",
-    phoneDiscription: "APPLE iPhone 14 (Purple, 128 GB)",
-    rating: { starRating: "4.7", totalRating: 1560, reviews: 59844 },
-    phoneDetails: [
-      "256 GB ROM",
-      "11.94 cm (4.7 inch) Retina HD Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "A13 Bionic Chip with 3rd Gen Neural Engine Processor",
-      "Water and Dust Resistant (1 meter for Upto 30 minutes, IP67)",
-      "Fast Charge Capable",
-    ],
-    price: { payPrice: 0, price: 80000, exchangePrice: 23000, discount: 7 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "Samsung",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/0/h/k/-original-imagcg22czc3ggvw.jpeg?q=70",
-    phoneDiscription: "SAMSUNG Galaxy F23 5G (Aqua Blue, 128 GB)",
-    rating: { starRating: "4.7", totalRating: 1560, reviews: 59844 },
-    phoneDetails: [
-      "256 GB ROM",
-      "11.94 cm (4.7 inch) Retina HD Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "A13 Bionic Chip with 3rd Gen Neural Engine Processor",
-      "Water and Dust Resistant (1 meter for Upto 30 minutes, IP67)",
-      "Fast Charge Capable",
-    ],
-    price: {
-      payPrice: 0,
-      price: 40000,
-      exchangePrice: 14000,
-      discount: 15,
-    },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "Samsung",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/a/i/v/-original-imagfhu6bdzhnmkz.jpeg?q=70",
-    phoneDiscription: "SAMSUNG Galaxy F13 (Nightsky Green, 128 GB)",
-    rating: { starRating: "4.5", totalRating: 1550, reviews: 59864 },
-    phoneDetails: [
-      "4 GB RAM | 64 GB ROM | Expandable Upto 1 TB",
-      "16.76 cm (6.6 inch) Full HD+ Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-      "Fast Charge Capable",
-    ],
-    price: {
-      payPrice: 0,
-      price: 30000,
-      exchangePrice: 12000,
-      discount: 12,
-    },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "Samsung",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/9/n/h/galaxy-m04-sm-m045f-samsung-original-imagh8ghfyxznzyu.jpeg?q=70",
-    phoneDiscription: "SAMSUNG Galaxy M04 (Dark Blue, 64 GB)",
-    rating: { starRating: "4.1", totalRating: 3550, reviews: 23864 },
-    phoneDetails: [
-      "4 GB RAM | 64 GB ROM | Expandable Upto 1 TB",
-      "16.76 cm (6.6 inch) Full HD+ Display",
-      "16.51 cm (6.5 inch) HD+ Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-      "Fast Charge Capable",
-    ],
-    price: {
-      payPrice: 0,
-      price: 25000,
-      exchangePrice: 10000,
-      discount: 15,
-    },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "realme",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/l4ln8nk0/mobile/p/h/p/-original-imagfggryyzyvhgw.jpeg?q=70",
-    phoneDiscription: "realme C30 (Bamboo Green, 32 GB)",
-    rating: { starRating: "4.1", totalRating: 4550, reviews: 5864 },
-    phoneDetails: [
-      "3 GB RAM | 32 GB ROM | Expandable Upto 500 GB",
-      "16.76 cm (6.6 inch) Full HD+ Display",
-      "16.51 cm (6.5 inch) HD+ Display",
-      "12MP Rear Camera | 7MP Front Camera",
-      "Unisoc T612 Processor",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-    ],
-    price: { payPrice: 0, price: 30000, exchangePrice: 12000, discount: 12 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "realme",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/i/b/c/-original-imaghafug7zpnemh.jpeg?q=70",
-    phoneDiscription:
-      "Realme C30 - Locked with Airtel Prepaid (Denim Black, 32 GB)",
-    rating: { starRating: "3.9", totalRating: 9350, reviews: 45864 },
-    phoneDetails: [
-      "2 GB RAM | 32 GB ROM | Expandable Upto 1 TB",
-      "116.51 cm (6.5 inch) HD+ Display",
-      "16.51 cm (6.5 inch) HD+ Display",
-      "8MP Rear Camera | 5MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-    ],
-    price: { payPrice: 0, price: 45000, exchangePrice: 19000, discount: 22 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "realme",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/b/q/s/-original-imaghbeqtgzsfzvc.jpeg?q=70",
-    phoneDiscription: "realme 9i 5G (Metallica Gold, 64 GB)",
-    rating: { starRating: "4.5", totalRating: 12950, reviews: 34584 },
-    phoneDetails: [
-      "2 GB RAM | 32 GB ROM | Expandable Upto 1 TB",
-      "116.51 cm (6.5 inch) HD+ Display",
-      "16.51 cm (6.5 inch) HD+ Display",
-      "8MP Rear Camera | 5MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-    ],
-    price: { payPrice: 0, price: 30000, exchangePrice: 8000, discount: 20 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
+const totalPhoneCards = document.querySelector(".total-card-class");
 
-  {
-    brandName: "POCO",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/l19m93k0/mobile/v/c/i/-original-imagcv2dpkd75r6z.jpeg?q=70",
-    phoneDiscription: "POCO X4 Pro 5G (Laser Blue, 64 GB)",
-    rating: { starRating: "4.2", totalRating: 11050, reviews: 5084 },
-    phoneDetails: [
-      "2 GB RAM | 32 GB ROM | Expandable Upto 1 TB",
-      "116.51 cm (6.5 inch) HD+ Display",
-      "16.51 cm (6.5 inch) HD+ Display",
-      "8MP Rear Camera | 5MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-    ],
-    price: { payPrice: 0, price: 15000, exchangePrice: 7000, discount: 6 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-  {
-    brandName: "POCO",
-    phoneImage:
-      "https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/y/9/g/-original-imagnfzyhh8gz8sd.jpeg?q=70",
-    phoneDiscription: "POCO C55 (Cool Blue, 128 GB)",
-    rating: { starRating: "4.6", totalRating: 15245, reviews: 8024 },
-    phoneDetails: [
-      "6 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
-      "17.04 cm (6.71 inch) HD+ Display",
-      "50MP Rear Camera | 5MP Front Camera",
-      "8MP Rear Camera | 5MP Front Camera",
-      "1 Year Warranty Provided By the Manufacturer from Date of Purchase",
-      "5000 mAh Battery",
-    ],
-    price: { payPrice: 0, price: 12000, exchangePrice: 5000, discount: 18 },
-    offers: { assuard: "yes", bankOffer: "yes" },
-  },
-];
+(function () {
+  fetch(
+    "https://raw.githubusercontent.com/1hisham/traing-oct/main/sever-fipkart/totalAll.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      allCardsOfAllPhones(data);
+      setupFilter(data);
+    });
+})();
 
+function payPriceCalculation(item) {
+  let discountPrice = 0;
+  let payPrice = 0;
+  discountPrice = item.price.price * Number(item.price.discount / 100);
+  payPrice = item.price.price - Number(discountPrice);
+  return payPrice;
+}
+function allCardsOfAllPhones(items) {
+  let totalCard = "";
+  let cardHtml = "";
+  items.forEach((item) => {
+    let total = "";
+    item.phoneDetails.forEach((list) => {
+      let html = "";
+      html = ` <li>${list}</li>`;
+      total += html;
+    });
 
+    cardHtml = `   <div class="total-details-container">
+        <section class="sub-container-one">
+        <div class="image-container">
+            <img loading="eager" class="_396cs4" alt="Apple iPhone SE (White, 256 GB) (Includes EarPods, Power Adapter)" src=${
+              item.phoneImage
+            }>
+        
+        <div class="heart-svg-image">
+            <svg xmlns="http://www.w3.org/2000/svg" class="_1l0elc" width="16" height="16" viewBox="0 0 20 16"><path d="M8.695 16.682C4.06 12.382 1 9.536 1 6.065 1 3.219 3.178 1 5.95 1c1.566 0 3.069.746 4.05 1.915C10.981 1.745 12.484 1 14.05 1 16.822 1 19 3.22 19 6.065c0 3.471-3.06 6.316-7.695 10.617L10 17.897l-1.305-1.215z" fill="lightgray" class="eX72wL" stroke="#FFF" fill-rule="evenodd" opacity=".9"></path></svg>
+        </div>
+        </div>
+        <form>                                
+        <input type="checkbox" id="compare">
+        <label for="compare">Add To Compare</label>
+        </form>
+        </section>
+        <section class="one-and-two">
+        <section class="sub-container-two">
+        <div class="model-name">
+            <span>${item.phoneDiscription}</span>
+        </div>
+        <div class="model-rating-section">
+            <div class="rating-star">
+                <span>${item.rating.starRating}</span>
+                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMyIgaGVpZ2h0PSIxMiI+PHBhdGggZmlsbD0iI0ZGRiIgZD0iTTYuNSA5LjQzOWwtMy42NzQgMi4yMy45NC00LjI2LTMuMjEtMi44ODMgNC4yNTQtLjQwNEw2LjUuMTEybDEuNjkgNC4wMSA0LjI1NC40MDQtMy4yMSAyLjg4Mi45NCA0LjI2eiIvPjwvc3ZnPg==" class="_1wB99o">
+            </div>
+            <span class="total-rating"${item.totalRating} Rating $ ${
+      item.reviews
+    }Reviews</span>
+        </div>
+        <ul class="model-features">
+            ${total}
+        </ul>
+       
+        </section>
+        <section class="sub-containter-three">
+        <div class="price-details">
+            <div class="cost-and-discount">
+                <div class="pay-price">₹${payPriceCalculation(item)}</div>
+               <img  class="assured" height="21" src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png">
+                <div class="total-cost"><div class="cost">₹ ${
+                  item.price.price
+                }</div><span class="discount">${
+      item.price.discount
+    }%0ff</span></div>
+                <div class="delivery">Free delivery</div>
+            </div>
+            <div class="exchange-price">upto ₹${
+              item.price.exchangePrice
+            } off on Exchange</div>
+            <div class="bank">Bank offer</div>
+        </div>
+        </section>
+        </section>
+        </div>  
+        `;
+
+    totalCard += cardHtml;
+  });
+
+  totalPhoneCards.innerHTML = totalCard;
+}
+
+function applyFilter(data) {
+  let brandFilters = [];
+  let ratingFilters = [];
+  let priceFilter = [];
+
+  const brand1 = document.querySelector(".apple");
+  const brand2 = document.querySelector(".samsung");
+  const brand3 = document.querySelector(".realme");
+
+  if (brand1.checked === true) {
+    brandFilters.push("Apple");
+  }
+
+  if (brand2.checked === true) {
+    brandFilters.push("Samsung");
+  }
+
+  if (brand3.checked === true) {
+    brandFilters.push("realme");
+  }
+
+  const rate1 = document.querySelector(".four-above");
+  const rate2 = document.querySelector(".three-above");
+
+  if (rate1.checked === true) {
+    ratingFilters.push(4);
+  }
+  if (rate2.checked === true) {
+    ratingFilters.push(3);
+  }
+
+  if (brandFilters.length) {
+    data = brandFilter(data, brandFilters);
+    console.log(data);
+    
+  }
+  
+  if (ratingFilters.length) {
+    data = ratingFilter(data, ratingFilters);
+    console.log(data,"rating");
+    
+  }
+  
+  allCardsOfAllPhones(data);
+}
+
+function setupFilter(data) {
+  const brand1 = document.querySelector(".apple");
+  const brand2 = document.querySelector(".samsung");
+  const brand3 = document.querySelector(".realme");
+  
+  brand1.addEventListener("change", () => {
+    applyFilter(data);
+  });
+  brand2.addEventListener("change", () => {
+    applyFilter(data);
+  });
+  brand3.addEventListener("change", () => {
+    applyFilter(data);
+  });
+
+  const rate1 = document.querySelector(".four-above");
+  const rate2 = document.querySelector(".three-above");
+
+  rate1.addEventListener("change", () => {
+    applyFilter(data);
+  });
+  rate2.addEventListener("change", () => {
+    applyFilter(data);
+  });
+  // price filter
+  const minRange = document.querySelector(".min-price");
+  const maxRange = document.querySelector(".max-price");
+
+  minRange.addEventListener("change", () => {
+    applyFilter(data);
+  });
+
+  maxRange.addEventListener("change", () => {
+    applyFilter(data);
+  });
+}
+
+function removeElementFromArray(list, value) {
+  idx = list.findIndex((val) => {
+    return val === value;
+  });
+
+  if (idx === -1) return;
+
+  return list.splice(idx, 1);
+}
+
+function brandFilter(data, brands) {
+  console.log();
+  return data.filter((item) => {
+    return brands.includes(item.brandName);
+  });
+}
+
+function ratingFilter(data, ratings) {
+  return data.filter((item) => {
+    let match = false;
+    for (let i = 0; i < ratings.length; i++) {
+      if (item.rating.starRating >= ratings[i]) {
+        match = true;
+        break;
+      }
+    }
+    return match;
+  });
+}
